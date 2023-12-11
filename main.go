27727a981f73208f42b977af231ea2935e28a70b
marian1908 <marianaschulc@gmail.com>
2023-12-11 15:44:00 -0300

    Guardar mensajes en txt

--- main.go
+++ main.go
@@ -2,19 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"golang.org/x/net/websocket"
 )
 
 type ChatRoom struct {
-	users map[*User]bool
+	users    map[*User]bool
+	messages *os.File
 }
 
 func newChatRoom() *ChatRoom {
+	messages, err := os.OpenFile("messages.txt", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644) //func OpenFile(name string, flag int, perm FileMode) (*File, error)
+	if err != nil {
+		fmt.Println("Error newChatRoom:", err)
+		return nil
+	}
+
 	return &ChatRoom{
-		users: make(map[*User]bool),
+		users:    make(map[*User]bool),
+		messages: messages,
 	}
 }
 
@@ -26,6 +37,37 @@ func (r *ChatRoom) handleWs(ws *websocket.Conn) {
 	}
 
 	r.users[user] = true
+
+	info, err := os.Stat("messages.txt")
+	if err != nil {
+		fmt.Println("Error info:", err)
+		return
+	}
+
+	if info.Size() > 0 {
+		file, err := os.OpenFile("messages.txt", os.O_RDWR, 0644) //func OpenFile(name string, flag int, perm FileMode) (*File, error)
+		if err != nil {
+			fmt.Println("Error open:", err)
+			return
+		}
+
+		history := make([]byte, 1024)
+		_, err = file.Read(history) //func (f *File) Read(b []byte) (n int, err error)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error read:", err)
+				return
+			}
+		}
+
+		if _, err = user.ws.Write(history); err != nil { //func (ws *Conn) Write(msg []byte) (n int, err error)
+			fmt.Println("Error send:", err)
+			return
+		}
+
+		fmt.Println("Los mensajes anteriores han sido enviados a", ws.RemoteAddr(), ":\n", string(history))
+	}
+
 	r.listen(user)
 }
 
@@ -35,22 +77,29 @@ func (r *ChatRoom) listen(user *User) {
 		n, err := user.ws.Read(data) //func (ws *Conn) Read(msg []byte) (n int, err error)
 		if err != nil {
 			delete(r.users, user)
-			fmt.Println("El usuario", user.ws.RemoteAddr(), "se ha desconectado.")
-			fmt.Println(err)
+			if err == io.EOF {
+				fmt.Println("El usuario", user.ws.RemoteAddr(), "se ha desconectado. Quedan", len(r.users), "usuarios conectados.")
+			} else {
+				fmt.Println("Error ws:", err)
+			}
 			return
 		}
 
-		msg := data[:n]
+		msg := []byte(user.ws.RemoteAddr().String() + ": " + string(data[:n]) + "\n" + time.Now().String() + "\n")
 		fmt.Println(string(msg))
 		r.sendToAll(msg)
+
+		if _, err = r.messages.WriteString(string(msg)); err != nil { //func (b *Writer) WriteString(s string) (int, error)
+			fmt.Println("Error write:", err)
+			return
+		}
 	}
 }
 
 func (r *ChatRoom) sendToAll(msg []byte) {
 	for user := range r.users {
-		_, err := user.ws.Write(msg) //func (ws *Conn) Write(msg []byte) (n int, err error)
-		if err != nil {
-			fmt.Println(err)
+		if _, err := user.ws.Write(msg); err != nil { //func (ws *Conn) Write(msg []byte) (n int, err error)
+			fmt.Println("Error sendAll:", err)
 			return
 		}
 	}
